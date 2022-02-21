6d7db36545928a241bd1ad500d4729e633b00730
theworldisburningRUN <89845610+theworldisburningRUN@users.noreply.github.com>
2022-02-21 09:13:05 -0500

    added achievements and base menue

--- clicker.js
+++ clicker.js
@@ -122,6 +122,10 @@ var clicker = {
             name: "Clickatroneronarator 5000™"
         },
     },
+    achievements:[{req: 'clicker.clicks > 0', had: false, text:'The first click', subtext:'\"it begins\"'},
+                 {req: 'clicker.upgrades.clickenator1000.amount > 0', had: false, text:'The first Clickenator™', subtext:'\"Not our top-of-the-line model, but certainly a best-seller.\"'},
+                 {req: 'clicker.clicks > 100', had: false, text:'', subtext:'\"it begins\"'},
+                 {req: 'clicker.upgrades.clickatron1000.amount > 0', had: false, text:'The Clickatron 1000™', subtext:'\"Our mid-range Clickatron™ series provides industry-standard clicking capacity, while still being lightweight and affordable.\"'},]
 };
 
 var delay = 0;
@@ -135,7 +139,7 @@ function clicked(thing) {
         update_upgrades();
     }
     else {
-        document.querySelector('body').innerHTML += `<div class="notifications">
+        document.querySelector('body').innerHTML += `<div class="notifications-warning">
         <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
         You can't afford that!
     </div>`;
@@ -147,121 +151,145 @@ function update_upgrades() {
     document.querySelector('#upgrades').innerHTML = '';
     for (i in clicker.upgrades) {
         document.querySelector('#upgrades').innerHTML += `<br> <button class="buy-upgrade" onclick="clicked('${i}')">${clicker.upgrades[i].name}</button> You have ${clicker.upgrades[i].amount} Cost: ${clicker.upgrades[i].cost}`;
-    }
-}
+    };
+};
 
-function updateClicksCount() {
-    if (Cookies.get("clicker") != null && Cookies.get("clicker") != "undefined") {
-        var clicker1 = JSON.parse(Cookies.get("clicker"));
-        for (i in clicker.upgrades) {
-            if (clicker1.upgrades[i] == null) {
-                clicker1.upgrades[i] = clicker.upgrades[i];
-            }
-        }
-        clicker = clicker1;
-    }
-    update_upgrades();
-    setInterval(() => {
-        for (i in clicker.upgrades) {
-            clicker.clicks += clicker.upgrades[i].amount * clicker.upgrades[i].cps / 20;
-        }
-        document.querySelector(".counter").innerHTML = "You have " + String(clicker.clicks).split('.')[0] + " clicks.";
-        update_quote_and_stuff();
-        delay++;
-        if (delay >= 40) {
-            Cookies.set("clicker", JSON.stringify(clicker), {expires: 100000});
-            delay = 0;
-        }
-    }, 50);
-}
+
+/*  >:D  */
+function updateClicksCount() {;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;if (Cookies.get("clicker") != null && Cookies.get("clicker") != "undefined") {;
+;;;;;;;;var clicker1 = JSON.parse(Cookies.get("clicker"));;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;for (i in clicker.upgrades) {;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;if (clicker1.upgrades[i] == null) {;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;clicker1.upgrades[i] = clicker.upgrades[i];;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;};;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;};;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;clicker = clicker1;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+
+;;;;;;;;for (i in clicker.achievements) {
+;;;;;;;;;;;;if (clicker1.achievements[i] == null || clicker.achievements[i].text != clicker1.achievements[i].text){
+;;;;;;;;;;;;;;;;clicker1.achievements[i] = clicker.achievements[i];
+;;;;;;;;;;;;};
+;;;;;;;;};
+;;;;;;;;clicker = clicker1;
+;;;;};
+
+;;;;update_upgrades();
+;;;;setInterval(() => {
+;;;;;;;;for (i in clicker.upgrades) {
+;;;;;;;;;;;;clicker.clicks += clicker.upgrades[i].amount * clicker.upgrades[i].cps / 20;
+;;;;;;;;};
+;;;;;;;;for (i in clicker.achievements) {
+;;;;;;;;;;;;var b = new Function('return '+clicker.achievements[i].req);
+;;;;;;;;;;;;if (b() && !clicker.achievements[i].gotten) {
+;;;;;;;;;;;;;;;;clicker.achievements[i].gotten = true;
+;;;;;;;;;;;;;;;;document.querySelector('body').innerHTML += `<div class="notifications-achievement"><span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>${clicker.achievements[i].text}<p5 class='quote-achievement'>${clicker.achievements[i].subtext}</p5></div>`;
+;;;;;;;;;;;;}
+;;;;;;;;};
+;;;;;;;;document.querySelector(".counter").innerHTML = "You have " + String(clicker.clicks).split('.')[0] + " clicks.";
+;;;;;;;;update_quote_and_stuff();
+;;;;;;;;delay++;
+;;;;;;;;if (delay >= 40) {
+;;;;;;;;;;;;Cookies.set("clicker", JSON.stringify(clicker), {expires: 100000});
+;;;;;;;;;;;;delay = 0;
+;;;;;;;;}
+;;;;}, 50);
+};
 
 function update_quote_and_stuff() {
     if (clicker.clicks < 0) {
-        document.querySelector('.quote').innerHTML = "\"How did you manage that?!?\""
-    }
+        document.querySelector('.quote').innerHTML = "\"How did you manage that?!?\"";
+    };
     if (clicker.clicks > 0) {
-        document.querySelector('.quote').innerHTML = "\"A good start.\""
-    }
+        document.querySelector('.quote').innerHTML = "\"A good start.\"";
+    };
     if (clicker.clicks > 10) {
-        document.querySelector('.quote').innerHTML = "\"You got this!\""
-    }
+        document.querySelector('.quote').innerHTML = "\"You got this!\"";
+    };
     if (clicker.clicks > 20) {
-        document.querySelector('.quote').innerHTML = "\"Keep pushing...\""
-    }
+        document.querySelector('.quote').innerHTML = "\"Keep pushing...\"";
+    };
     if (clicker.clicks == 50) {
-        document.querySelector('.quote').innerHTML = "\"Half of the way to 100!\""
-    }
+        document.querySelector('.quote').innerHTML = "\"Half of the way to 100!\"";
+    };
     if (clicker.clicks > 50) {
-        document.querySelector('.quote').innerHTML = "\"More than half of the way to 100!\""
-    }
+        document.querySelector('.quote').innerHTML = "\"More than half of the way to 100!\"";
+    };
     if (clicker.clicks > 80) {
-        document.querySelector('.quote').innerHTML = "\"Almost there...\""
-    }
+        document.querySelector('.quote').innerHTML = "\"Almost there...\"";
+    };
     if (clicker.clicks > 90) {
-        document.querySelector('.quote').innerHTML = "\"Nearly...\""
-    }
+        document.querySelector('.quote').innerHTML = "\"Nearly...\"";
+    };
     if (clicker.clicks == 100) {
-        document.querySelector('.quote').innerHTML = "\"100 clicks!\""
-    }
+        document.querySelector('.quote').innerHTML = "\"100 clicks!\"";
+    };
     if (clicker.clicks > 100) {
-        document.querySelector('.quote').innerHTML = "\"You did it!\""
-    }
+        document.querySelector('.quote').innerHTML = "\"You did it!\"";
+    };
     if (clicker.clicks > 105) {
-        document.querySelector('.quote').innerHTML = "\"Nice!\""
-    }
+        document.querySelector('.quote').innerHTML = "\"Nice!\"";
+    };
     if (clicker.clicks > 150) {
-        document.querySelector('.quote').innerHTML = "\"I'm c, by the way.\""
-    }
+        document.querySelector('.quote').innerHTML = "\"I'm c, by the way.\"";
+    };
     if (clicker.clicks > 200) {
-        document.querySelector('.quote').innerHTML = "\"Nice to meet you!\""
-    }
+        document.querySelector('.quote').innerHTML = "\"Nice to meet you!\"";
+    };
     if (clicker.clicks > 280) {
-        document.querySelector('.quote').innerHTML = "\"You can reach me at cplus1.almondsaretasty@gmail.com\""
-    }
+        document.querySelector('.quote').innerHTML = "\"You can reach me at cplus1.almondsaretasty@gmail.com\"";
+    };
     if (clicker.clicks > 450) {
-        document.querySelector('.quote').innerHTML = "\"Also, check out <a href=\"https://euphoria.io/room/xkcd\">euphoria</a>!\""
-    }
+        document.querySelector('.quote').innerHTML = "\"Also, check out <a href=\"https://euphoria.io/room/xkcd\">euphoria</a>!\"";
+    };
     if (clicker.clicks > 1000) {
-        document.querySelector('.quote').innerHTML = "\"Must. Add. More. Levels.\""
-    }
+        document.querySelector('.quote').innerHTML = "\"Must. Add. More. Levels.\"";
+    };
     if (clicker.clicks > 5000) {
-        document.querySelector('.quote').innerHTML = "\"You certainly are a prolific clicker.\""
-    }
+        document.querySelector('.quote').innerHTML = "\"You certainly are a prolific clicker.\"";
+    };
     if (clicker.clicks > 7000) {
-        document.querySelector('.quote').innerHTML = "\"*clickity clickity clickity clickity *\""
-    }
+        document.querySelector('.quote').innerHTML = "\"*clickity clickity clickity clickity *\"";
+    };
     if (clicker.clicks > 10000) {
-        document.querySelector('.quote').innerHTML = "\"10,000 clicks!\""
-    }
+        document.querySelector('.quote').innerHTML = "\"10,000 clicks!\"";
+    };
     if (clicker.clicks == 69420) {
-        document.querySelector('.quote').innerHTML = "\"hehhehehehheheheheh funny number hehehhe\""
-        alert("FUNNY NUMBER ALERT")
-        sleep(2000)
-    }
+        document.querySelector('.quote').innerHTML = "\"hehhehehehheheheheh funny number hehehhe\"";
+        alert("FUNNY NUMBER ALERT");
+        sleep(2000);
+    };
     if (clicker.clicks > 100000) {
-        document.querySelector('.quote').innerHTML = "\"you fucking psychopath\""
-    }
+        document.querySelector('.quote').innerHTML = "\"you fucking psychopath\"";
+    };
     if (clicker.clicks > 500000) {
-        document.querySelector('.quote').innerHTML = "\"Keep pushing!\""
-    }
+        document.querySelector('.quote').innerHTML = "\"Keep pushing!\"";
+    };
     if (clicker.clicks > 1000000) {
-        document.querySelector('.quote').innerHTML = "\"Inspirational quotes set false expectations.\""
-    }
+        document.querySelector('.quote').innerHTML = "\"Inspirational quotes set false expectations.\"";
+    };
     if (clicker.clicks > 1500000) {
-        document.querySelector('.quote').innerHTML = "\"Pickles are cucumbers soaked in evil.\""
-    }
-}
+        document.querySelector('.quote').innerHTML = "\"Pickles are cucumbers soaked in evil.\"";
+    };
+};
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-}
+};
 
-document.querySelector('#version').innerHTML = "BETA 5.3"
 
 function addClick () {
-    clicker.clicks++
+    clicker.clicks++;
+};
+
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
 }
-
-
-
-
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
