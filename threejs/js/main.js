e1721b0a4cb9136526d2f27a3e5338f724bd6b26
theworldisburningRUN <89845610+theworldisburningRUN@users.noreply.github.com>
2022-02-27 19:50:17 -0500

    image

--- threejs/js/main.js
+++ threejs/js/main.js
@@ -1,18 +1,27 @@
-const scene = new THREE.Scene(); // Create scene
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); // Create camera with FOV of 75, aspect ratio of width/over height, near clipping plane of 0.1 and far clipping plane of 1000
+let scene, camera, renderer, cube;
 
-// Set up renderer
-const renderer = new THREE.WebGLRenderer({antialias:true});
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+function init() {
+    scene = new THREE.Scene(); // Create scene
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); // Create camera with FOV of 75, aspect ratio of width/over height, near clipping plane of 0.1 and far clipping plane of 1000
 
-// Create a cube
-const geometry = new THREE.BoxGeometry(1, 3, 1);
-const material = new THREE.MeshBasicMaterial( { color: 0x9000ff } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+    // Set up renderer
+    renderer = new THREE.WebGLRenderer({antialias:true});
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
 
-camera.position.z = 5; // Set the camera position
+    // Create a cube
+    const geometry = new THREE.BoxGeometry(1, 3, 1);
+    const material = new THREE.MeshBasicMaterial( { color: 0x9000ff } );
+
+    // const texture = new THREE.TextureLoader().load('./textures/euph-logo.png');
+    // const material = new THREE.MeshBasicMaterial( { map:texture } );
+
+
+    cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5; // Set the camera position
+}
 
 function animate() { // Draw the scene
 	requestAnimationFrame( animate );
@@ -23,4 +32,14 @@ function animate() { // Draw the scene
 
 	renderer.render( scene, camera );
 }
+
+function onResize() {
+    camera.aspect = window.innerWidth/window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+// Run
+init()
+window.addEventListener('resize', onResize(), false);
 animate();
